d5fa9bf414d21422cf73661845327ae9b647aeeb
DKZ <davidkingzyb@qq.com>
2020-01-08 11:07:20 +0800

    remove danmu

--- pccold/__init__.py
+++ pccold/__init__.py
@@ -1,5 +1,4 @@
 from .pccold import main
 from .videodownload import downloadVideo,download3DaysVideo
-from .danmu import Danmu
 from .tools import ReturnCodeObserverThread,SleepKillerThread
 from .wsdanmu import wsdanmumain
